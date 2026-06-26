fd3ad02180ed89d698e05b3f3c75124b2c477c00
dheermrt <dheer968793@gmail.com>
2026-06-26 15:28:55 +0530

    added sobel filters

--- pcv_get_filters.h
+++ pcv_get_filters.h
@@ -7,7 +7,7 @@ extern "C" {
 
 int get_filters(
     int n,
-    unsigned char *output,
+    float *output,
     char* filter
 );
 
@@ -17,23 +17,87 @@ int get_filters(
 
 #ifdef PCV_GET_FILTERS
 
-void get_canny(unsigned char (*out)[n])
+void get_pascal_row(float* smooth,int n){
+    float num=1.0f; 
+    float denum=1.0f;
+    smooth[0]=1; 
+    for(int i=1;i<n;i++){
+        num=num*((float)(n-i));
+        denum=denum*i; 
+        smooth[i]=num/denum;
+    }
+}
 
+void get_derv(float* derv,int n){
+    int i;
+    float smoother[n - 2];
+
+    get_pascal_row(smoother, n - 2);
+    for (i = 0; i < n; ++i) {
+        float left = 0.0f;
+        float right = 0.0f;
+        if (i >= 2) {
+            left = smoother[i - 2];
+        }
+        if (i < (n - 2)) {
+            right = smoother[i];
+        }
+        derv[i] = left - right;
+    }
+}
+
+void get_sobel_x(float (*out)[n],int n){
+    float dev[n]; 
+    float smooth[n];
+    if(n==3){
+        dev[0]=-1; 
+        dev[1]=0 ;
+        dev[2]=-1; 
+        smooth[0]=1; 
+        smooth[1]=2; 
+        smooth[2]=1; 
+    }
+    else{
+        get_pascal_row(smooth,n); 
+        get_derv(dev,n); 
+    }
+    for(int y=0;y<n;y++){
+        for(int x=0;x<n;x++){
+            out[y][x]=smooth[y]*dev[x];
+        }
+    }
+}
+void get_sobel_y(float(*out)[n],int n){
+    get_sobel_x(out,n); 
+    for(int y=0;y<n;y++){
+        for(int x=y+1;x<n;x++){
+            float temp=out[y][x];
+            out[y][x]=out[x][y]; 
+            out[x][y]=temp;
+             
+        }
+    }
+}
 int get_filters(
     int n,
     unsigned char *output, 
     unsigned char* filter
 )
-unsigned char (*out)[n]=(unsigned char(*)[n])output;
+float (*out)[n]=(float(*)[n])output;
 
-if(strncmp(filter,"CANNY",5)==0){
-
+if(strncmp(filter,"BOX",5)==0){
+    const float denum=1/16.0f;
+    for(int y=0;y<n;y++){
+        for(int x=0;x<n;x++){
+            out[y][x]=denum; 
+        }
+    }
 }
 else if(strncmp(filter,"SOBEL_X",7)==0){
-
+    get_sobel_x(out,n); 
 }
 else if(strncmp(filter,"SOBEL_Y")==0){
-
+    get_sobel_y(out,n); 
 }
 else if(strncmp(filter,"GAUSSIAN_BLUR",13)==0){
 
